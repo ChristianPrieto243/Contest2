cbc6a0b3d4ec86195db739df576b1e77b4aa0de0
Nicholas Shenk <nickshenk68@gmail.com>
2025-11-25 10:54:36 -0500

    add include statements Experimental.c

--- Experimental.c
+++ Experimental.c
@@ -1,1 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <math.h>
+#include <windows.h>
+#include <windowsx.h>
 
+#define min(a,b) (((a) < (b)) ? (a) : (b))
+#define max(a,b) (((a) > (b)) ? (a) : (b))
+// COMPILE : gcc Experimental.c -o Experiment -mwindows
