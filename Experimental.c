d919b95136c9cd40417429c0434cabf4c4055f99
Nicholas Shenk <nickshenk68@gmail.com>
2025-11-25 10:59:25 -0500

    Basic winproc Experimental.c

--- Experimental.c
+++ Experimental.c
@@ -60,3 +60,13 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
 
     return 0;
 }
+LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam) {
+    switch (uMsg) {
+        case WM_LBUTTONDOWN:
+            break;
+        case WM_DESTROY:
+            PostQuitMessage(0);
+            return 0;
+        }
+    return DefWindowProc(hwnd, uMsg, wParam, lParam);
+}
