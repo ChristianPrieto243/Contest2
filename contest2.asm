d950b96d12c45246908f1e097abbd73cd483f36e
Nicholas Shenk <nickshenk68@gmail.com>
2025-12-03 20:45:11 -0500

    Change elipse to a circle

--- contest2.asm
+++ contest2.asm
@@ -74,7 +74,7 @@ DrawAllTargets PROC
     ; Ellipse parameters: hdc, left, top, right, bottom
     ; For circle centered at (100,100) with radius 30:
     ; left = 70, top = 70, right = 130, bottom = 130
-    INVOKE Ellipse, hdc, 70, 70, 130, 130
+    INVOKE Ellipse, hdc, targetSize, targetSize, targetSize, targetSize
     
     ; TODO: Loop through targets array and draw all
     ; TODO: Change color based on target health
@@ -185,3 +185,4 @@ Exit_Program:
 WinMain ENDP
 
 END WinMain
+
